5efd54e49de780700cd8af2039dff1579c75483c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-04 08:33:16 +0100

    Upgrade to lts-21.22 and nixos-23.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     extra-trusted-public-keys = "safe-coloured-text.cachix.org-1:BriaRVzOr8kxni3hMCw/BRhKx85SkltChBw6PVdt884=";
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
+    nixpkgs-23_05.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
     nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
     nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
     nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
@@ -24,6 +25,7 @@
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-23_05
     , nixpkgs-22_11
     , nixpkgs-22_05
     , nixpkgs-21_11
@@ -58,6 +60,7 @@
           backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).safeColouredTextRelease;
           allNixpkgs = {
             inherit
+              nixpkgs-23_05
               nixpkgs-22_11
               nixpkgs-22_05
               nixpkgs-21_11;
